e05104ec53a9f923835e1c964c89a03783f67d57
ionOM <ionblockchain@gmail.com>
2023-08-05 14:10:13 +0200

    first commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,32 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Ion Platon
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ */
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    errot DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burb(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        
+    }
+}   
